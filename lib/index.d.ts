b7b98eddbd55cda68ba60fe2b205967ad725fccf
Arno Fiva <arnofiva@gmail.com>
2019-08-19 07:55:46 +0200

    Deploy latest changes

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="arcgis-js-api" />
+declare const _default: {
+    interpolate: <T extends number | number[] | __esri.Point | __esri.Camera>(a: T, b: T, t: number, total?: number | undefined, modulo?: number | undefined) => T | undefined;
+};
+export = _default;
---
+++ lib/support/interpolate.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="arcgis-js-api" />
+import * as Camera from "esri/Camera";
+import * as Point from "esri/geometry/Point";
+declare type LerpObject = Point | Camera;
+declare type LerpValue = number | number[] | LerpObject;
+declare const lerp: <T extends LerpValue>(a: T, b: T, t: number, total?: number | undefined, modulo?: number | undefined) => T | undefined;
+export = lerp;
