212a5e82e3b37c4bef91c8945f2ead02191fea87
Arno Fiva <arnofiva@gmail.com>
2019-08-20 19:16:06 +0200

    Added support for interpolating dates

--- src/ts/support/interpolate.ts
+++ src/ts/support/interpolate.ts
@@ -1,10 +1,10 @@
-
-
 import * as Camera from "esri/Camera";
 import * as Point from "esri/geometry/Point";
 
+
+
 type LerpObject = Point | Camera;
-type LerpValue = number | number[] | LerpObject;
+type LerpValue = number | number[] | Date | LerpObject;
 
 const delta = (a: number, b: number, modulo?: number): number => {
   let d = b - a;
@@ -40,9 +40,9 @@ const objectLerp = <T extends LerpObject>(a: T, b: T, t: number, total?: number,
       const cameraA = a as Camera;
       const cameraB = b as Camera;
       return new Camera({
-        heading: lerp(cameraA.heading, cameraB.heading, t, total, 360),
-        position: lerp(cameraA.position, cameraB.position, t, total),
-        tilt: lerp(cameraA.tilt, cameraB.tilt, t, total),
+        heading: numberLerp(cameraA.heading, cameraB.heading, t, total, 360),
+        position: objectLerp(cameraA.position, cameraB.position, t, total),
+        tilt: numberLerp(cameraA.tilt, cameraB.tilt, t, total),
       }) as T;
     } else if (a.declaredClass === "esri.geometry.Point") {
       const pointA = a as Point;
@@ -50,9 +50,9 @@ const objectLerp = <T extends LerpObject>(a: T, b: T, t: number, total?: number,
       const spatialReference = pointA.spatialReference;
       return new Point({
         spatialReference,
-        x: lerp(pointA.x, pointB.x, t, total, modulo),
-        y: lerp(pointA.y, pointB.y, t, total, modulo),
-        z: lerp(pointA.z, pointB.z, t, total, modulo),
+        x: numberLerp(pointA.x, pointB.x, t, total, modulo),
+        y: numberLerp(pointA.y, pointB.y, t, total, modulo),
+        z: numberLerp(pointA.z, pointB.z, t, total, modulo),
       }) as T;
     }
   }
@@ -64,6 +64,16 @@ const arrayLerp = (a: number[], b: number[], t: number, total?: number, modulo?:
     return a.map((value, index) => lerp(value, b[index], t, total, modulo)) as any;
   }
   throw new Error("Value arrays must have same length for interpolation");
+}
+
+const isDate = (date: any): boolean => {
+  return date && Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date);
+}
+
+const dateLerp = (a: Date, b: Date, t: number, total?: number): Date => {
+  const aTime = a.getTime();
+  const bTime = b.getTime();
+  return new Date(numberLerp(aTime, bTime, t, total));
 }
 
 const lerp = <T extends LerpValue>(a: T, b: T, t: number, total?: number, modulo?: number): T | undefined => {
@@ -78,6 +88,8 @@ const lerp = <T extends LerpValue>(a: T, b: T, t: number, total?: number, modulo
     } else if (typeofA === "object") {
       if (Array.isArray(a) && Array.isArray(b)) {
         return arrayLerp(a, b, t, total, modulo) as T;
+      } else if (isDate(a) && isDate(b)) {
+        return dateLerp(a as Date, b as Date, t, total) as T;
       } else {
         return objectLerp(a as LerpObject, b as LerpObject, t, total) as T;
       }
